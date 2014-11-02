580ea9d0323122dac758e760f1ccd6802485d3bb
alexzkazu <alex.ziebol@gmail.com>
2014-11-01 18:09:05 -0700

    include config, for local use

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+	mongo: {
+		address: 'mongodb://alexzkazu:uplifty@ds047440.mongolab.com:47440/uplifty'
+	},
+	redis: {
+		host: 'pub-redis-14532.eu-west-1-1.2.ec2.garantiadata.com',
+		port: 14532,
+		pass: 'test-rd'
+	}
+};
